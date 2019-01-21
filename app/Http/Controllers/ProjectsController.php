cb5075f4b52b9c37c855ca99837b6ffe74e1e1f1
Scott Horn <shorn813@outlook.com>
2019-01-21 14:49:17 -0500

    adding authetication

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -6,8 +6,14 @@ use App\Project;
 
 class ProjectsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
-        $projects = Project::all();
+        $projects = Project::where('owner_id', auth()->id())->get();
 
         return view('projects.index', compact('projects'));
     }
@@ -46,6 +52,8 @@ class ProjectsController extends Controller
             'title' => 'required',
             'description' => 'required'
         ]);
+
+        $attributes['owner_id'] = auth()->id();
 
         Project::create($attributes);
 
